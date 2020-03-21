65fba18d2b7e03b28cdbc3a3d8ef4cf6cf4854d7
Tao0Lu <2298273530@qq.com>
2020-03-21 21:37:20 +0800

    EX

---
+++ src/main/java/arknights/registry/EntityHandler.java
@@ -0,0 +1,30 @@
+package arknights.registry;
+
+import arknights.entity.*;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.IForgeRegistry;
+
+import static arknights.Arknights.MODID;
+
+public class EntityHandler {
+    public static final EntityType<DisasterZero> DISASTERZERO = (EntityType<DisasterZero>) EntityType.Builder.<DisasterZero>create(DisasterZero::new, EntityClassification.MISC).build("disaster_zero").setRegistryName(MODID + ":disaster_zero");
+    public static final EntityType<SnowStorm> SNOWSTORM = (EntityType<SnowStorm>) EntityType.Builder.<SnowStorm>create(SnowStorm::new, EntityClassification.MISC).build("snowstorm").setRegistryName(MODID + ":snowstorm");
+
+    public static final EntityType<Meteorite> METEORITE = (EntityType<Meteorite>)EntityType.Builder.<Meteorite>create(Meteorite::new, EntityClassification.MISC).build("meteorite").setRegistryName(MODID + ":" + "meteorite");
+
+    public static final EntityType<OriginiumSlugEntity> ORIGINIUMSLUG = (EntityType<OriginiumSlugEntity>)EntityType.Builder.<OriginiumSlugEntity>create(OriginiumSlugEntity::new, EntityClassification.MONSTER).build("originium_slug").setRegistryName(MODID + ":" + "originium_slug");
+    private static final EntityType<ExusiaiEntity> EXUSIAI = (EntityType<ExusiaiEntity>)EntityType.Builder.<ExusiaiEntity>create(ExusiaiEntity::new, EntityClassification.MONSTER).build("exusiai").setRegistryName(MODID + ":" + "exusiai");;
+
+    @SubscribeEvent
+    public static void register(RegistryEvent.Register<EntityType<?>> evt){
+        IForgeRegistry<EntityType<?>> r = evt.getRegistry();
+        r.register(DISASTERZERO);
+        r.register(SNOWSTORM);
+        r.register(METEORITE);
+        r.register(ORIGINIUMSLUG);
+		r.register(EXUSIAI);
+    }
+}
