388bb6156d47133562693d1db5dcff0720994098
南宫逸枫 <1605146531@qq.com>
2020-03-28 21:59:00 +0800

    Exusiai shooting

--- src/main/java/arknights/entity/ExusiaiEntity.java
+++ src/main/java/arknights/entity/ExusiaiEntity.java
@@ -61,24 +61,24 @@ public class ExusiaiEntity extends OperatorBase implements IRangedAttackMob{
 
     public void attackEntityWithRangedAttack(LivingEntity target, float var2){
         if(target != this.getOwner()) {
-            double d0 = target.func_226277_ct_() - this.func_226277_ct_();
-            double d1;
-            double d2 = target.func_226281_cx_() - this.func_226281_cx_();
-            double d3 = (double) MathHelper.sqrt(d0 * d0 + d2 * d2);
-            //float f = MathHelper.sqrt(d1 * d1 + d3 * d3) * 0.2F;
+            double deltaX = target.func_226277_ct_() - this.func_226277_ct_();
+            double deltaY;
+            double deltaZ = target.func_226281_cx_() - this.func_226281_cx_();
+            double distance = (double) MathHelper.sqrt(deltaX * deltaX + deltaZ * deltaZ);
+            //float f = MathHelper.sqrt(deltaY * deltaY + distance * distance) * 0.2F;
 
             if (!this.isSkill && !world.isRemote()) {
                 BulletEntity bulletEntity = new BulletEntity(this, this.world);
-                d1 = target.func_226283_e_(0.3333333333333333D) - bulletEntity.func_226278_cu_();
-                bulletEntity.shoot(d0, d1 + d3 * (double) 0.2F, d2, 1.6F, 1.0F);
+                deltaY = target.func_226283_e_(0.3333333333333333D) - bulletEntity.func_226278_cu_();
+                bulletEntity.shoot(deltaX, deltaY, deltaZ, 1.6F, 1.0F);
                 this.playSound(SoundHandler.EXUSIAI_ATTACK, 1.0F, 1.0F);
                 this.world.addEntity(bulletEntity);
             } else if (this.isSkill) {
                 for (int i = 0; i < 5; i++) {
                     BulletEntity bulletEntity = new BulletEntity(this, this.world);
-                    d1 = target.func_226283_e_(0.3333333333333333D) - bulletEntity.func_226278_cu_();
-                    //float f = MathHelper.sqrt(d1 * d1 + d3 * d3) * 0.2F;
-                    bulletEntity.shoot(d0, d1 + d3 * (double) 0.2F, d2, 1.6F, 1.0F);
+                    deltaY = target.func_226283_e_(0.3333333333333333D) - bulletEntity.func_226278_cu_();
+                    //float f = MathHelper.sqrt(deltaY * deltaY + distance * distance) * 0.2F;
+                    bulletEntity.shoot(deltaX, deltaY, deltaZ, 1.6F, 1.0F);
                     this.world.addEntity(bulletEntity);
                 }
                 this.playSound(SoundHandler.EXUSIAI_SKILLATTACK, 1.0F, 1.0F);
